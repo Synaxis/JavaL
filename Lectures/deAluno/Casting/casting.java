1427b3c3bc18122779d15fd73df429af00ec1f55
Synaxis <oiacow@gmail.com>
2018-10-12 00:14:21 -0300

    casting

---
+++ Lectures/deAluno/Casting/casting.java
@@ -0,0 +1,21 @@
+
+package casting2;
+
+
+public class casting {
+	public static void main(String[] src) {
+		int x = 10;
+		int y = 6;
+		int z = 3;
+		double a = 3.0; double b = 1.0;
+		double c = 1.5;
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+		System.out.println("z="+z);
+		
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("c="+c);
+	}
+}
+
